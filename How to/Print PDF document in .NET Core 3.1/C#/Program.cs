183c89b3719ceb70ca3f71adaf367ea114ca84fc
DeepakG <deepak21191@gmail.com>
2020-05-13 11:15:29 +0530

    "How to:" Folder is created. .Net core 3.1 printing sample is added.

---
+++ How to/Print PDF document in .NET Core 3.1/C#/Program.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.Windows.Forms.PdfViewer;
+using System.Windows.Forms;
+
+namespace PDFPrintDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PdfDocumentView viewer = new PdfDocumentView();
+
+            //Load the PDF document
+            viewer.Load("../../../Data/Barcode.pdf");
+
+            //Initialize print dialog.
+            PrintDialog dialog = new PrintDialog();
+
+            dialog.AllowPrintToFile = true;
+
+            dialog.AllowSomePages = true;
+
+            dialog.AllowCurrentPage = true;
+
+            dialog.Document = viewer.PrintDocument;
+
+            //Print the PDF document
+            dialog.Document.Print();
+
+            //Dispose the viewer
+            viewer.Dispose();
+        }
+    }
+}
