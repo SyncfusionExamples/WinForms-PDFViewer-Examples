619cfa3e47d82a66f698de01610949d4fe38a57f
DeepakG <deepak21191@gmail.com>
2020-05-14 13:06:22 +0530

    Save Events demo is added

---
+++ Save/SaveEventsDemo/Form1.cs
@@ -0,0 +1,40 @@
+using System.Windows.Forms;
+using Syncfusion.Windows.Forms.PdfViewer;
+
+namespace SaveEventsDemo
+{
+    public partial class Form1 : Form
+    {
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Wire the `BeginSave` event.
+            pdfViewerControl1.BeginSave += PdfViewerControl1_BeginSave;
+
+            //Wire the `EndSave` event.
+            pdfViewerControl1.EndSave += PdfViewerControl1_EndSave;
+
+            //Load the PDF file.
+#if NETCORE
+            pdfViewerControl1.Load("../../../Data/HTTP Succinctly.pdf");
+#else
+            pdfViewerControl1.Load("../../Data/HTTP Succinctly.pdf");
+#endif
+        }
+        #endregion
+
+        #region Events
+        private void PdfViewerControl1_BeginSave(object sender, BeginSaveEventArgs e)
+        {
+            //Insert your code here    
+        }
+
+        private void PdfViewerControl1_EndSave(object sender, EndSaveEventArgs e)
+        {
+            //Insert your code here
+        }
+        # endregion
+    }
+}
--- Save/SaveEventsDemo/Program.cs
+++ Save/SaveEventsDemo/Program.cs
@@ -8,7 +8,7 @@
 using System;
 using System.Windows.Forms;
 
-namespace PrintEventsDemo
+namespace SaveEventsDemo
 {
     static class Program
     {
