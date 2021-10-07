97cf07a072a710e1805b9608803577bfed29ce62
Deepak G <65169077+Deepak1799@users.noreply.github.com>
2021-10-07 23:40:48 +0530

    Bookmarks Navigation Example is committed

---
+++ Bookmarks Navigation/PDFBookmarks/PDFBookmarks/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Windows.Forms.PdfViewer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SampleWF
+{
+    public partial class Form1 : Form
+    {
+        PdfViewerControl pdfViewer;
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+
+            pdfViewer = new PdfViewerControl();
+            pdfViewer.Dock= DockStyle.Fill;
+            this.Controls.Add(pdfViewer);
+            
+            // wire the document loaded event.
+            pdfViewer.DocumentLoaded += PdfViewer_DocumentLoaded;
+
+            // load the PDF.
+            pdfViewer.Load("../../Data/C_Sharp_Succinctly.pdf");
+        }
+
+        /// <summary>
+        /// Navigates the control to a bookmark
+        /// </summary>
+        void GoToBookmark()
+        {
+            PdfLoadedDocument pdfLoadedDocument = pdfViewer.LoadedDocument;
+
+            //Get the complete bookmarks in the PDF.
+            PdfBookmarkBase bookmarks = pdfLoadedDocument.Bookmarks;
+
+            //Get the first bookmark.
+            PdfBookmark firstBookmark = bookmarks[0];
+
+            //Navigates to the first bookmark present in the PDF.
+            pdfViewer.GoToBookmark(firstBookmark);
+        }
+
+        /// <summary>
+        /// Navigates the control to the child bookmark of a bookmark
+        /// </summary>
+        void GoToChildBookmark()
+        {
+            PdfLoadedDocument pdfLoadedDocument = pdfViewer.LoadedDocument;
+
+            //Get the complete bookmarks in the PDF.
+            PdfBookmarkBase bookmarks = pdfLoadedDocument.Bookmarks;
+
+            //Gets the fourth bookmark in the PDF.
+            PdfBookmark thirdBookmark = bookmarks[3];
+
+            //Check whether it has child bookmarks.
+            if (thirdBookmark.Count > 0)
+            {
+                //Navigates to the first child of the fourth bookmark in the PDF.
+                pdfViewer.GoToBookmark(bookmarks[3][0]);
+            }
+        }
+
+        private void PdfViewer_DocumentLoaded(object sender, EventArgs args)
+        {
+            //Call the logic to go to bookmark
+            GoToBookmark();
+        }
+    }
+}
---
+++ Bookmarks Navigation/README.md
@@ -0,0 +1,2 @@
+# WinForms-PDFViewer-Examples
+This repository contains various example projects of WinForms PDF Viewer control.
