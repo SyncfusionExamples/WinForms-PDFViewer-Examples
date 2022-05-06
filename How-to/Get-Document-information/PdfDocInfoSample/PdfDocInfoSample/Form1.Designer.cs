bfa8160934e4c1aed6a188131a59090f02cae3f2
Divya <divya.dhayalan@syncfusion.com>
2022-05-06 11:21:04 +0530

    WF-59457 - Include the example project for the documentation “How to get document information”.

---
+++ How-to/Get-Document-information/PdfDocInfoSample/PdfDocInfoSample/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace PdfDocInfoSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings messageBoxSettings2 = new Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings();
+            Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings pdfViewerPrinterSettings2 = new Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings textSearchSettings2 = new Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings();
+            this.pdfViewer = new Syncfusion.Windows.Forms.PdfViewer.PdfViewerControl();
+            this.SuspendLayout();
+            // 
+            // pdfViewer
+            // 
+            this.pdfViewer.CursorMode = Syncfusion.Windows.Forms.PdfViewer.PdfViewerCursorMode.SelectTool;
+            this.pdfViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfViewer.EnableContextMenu = true;
+            this.pdfViewer.EnableNotificationBar = true;
+            this.pdfViewer.HorizontalScrollOffset = 0;
+            this.pdfViewer.IsBookmarkEnabled = true;
+            this.pdfViewer.IsTextSearchEnabled = true;
+            this.pdfViewer.IsTextSelectionEnabled = true;
+            this.pdfViewer.Location = new System.Drawing.Point(0, 0);
+            messageBoxSettings2.EnableNotification = true;
+            this.pdfViewer.MessageBoxSettings = messageBoxSettings2;
+            this.pdfViewer.MinimumZoomPercentage = 50;
+            this.pdfViewer.Name = "pdfViewer";
+            this.pdfViewer.PageBorderThickness = 1;
+            pdfViewerPrinterSettings2.PageOrientation = Syncfusion.Windows.PdfViewer.PdfViewerPrintOrientation.Auto;
+            pdfViewerPrinterSettings2.PageSize = Syncfusion.Windows.PdfViewer.PdfViewerPrintSize.ActualSize;
+            pdfViewerPrinterSettings2.PrintLocation = ((System.Drawing.PointF)(resources.GetObject("pdfViewerPrinterSettings2.PrintLocation")));
+            pdfViewerPrinterSettings2.ShowPrintStatusDialog = true;
+            this.pdfViewer.PrinterSettings = pdfViewerPrinterSettings2;
+            this.pdfViewer.ReferencePath = null;
+            this.pdfViewer.ScrollDisplacementValue = 0;
+            this.pdfViewer.ShowHorizontalScrollBar = true;
+            this.pdfViewer.ShowToolBar = true;
+            this.pdfViewer.ShowVerticalScrollBar = true;
+            this.pdfViewer.Size = new System.Drawing.Size(800, 450);
+            this.pdfViewer.SpaceBetweenPages = 8;
+            this.pdfViewer.TabIndex = 0;
+            this.pdfViewer.Text = "pdfViewer";
+            textSearchSettings2.CurrentInstanceColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(255)))), ((int)(((byte)(171)))), ((int)(((byte)(64)))));
+            textSearchSettings2.HighlightAllInstance = true;
+            textSearchSettings2.OtherInstanceColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.pdfViewer.TextSearchSettings = textSearchSettings2;
+            this.pdfViewer.VerticalScrollOffset = 0;
+            this.pdfViewer.VisualStyle = Syncfusion.Windows.Forms.PdfViewer.VisualStyle.Default;
+            this.pdfViewer.ZoomMode = Syncfusion.Windows.Forms.PdfViewer.ZoomMode.Default;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pdfViewer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.PdfViewer.PdfViewerControl pdfViewer;
+    }
+}
+
---
+++ How-to/Get-Document-information/PdfDocInfoSample/PdfDocInfoSample/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace PdfDocInfoSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Loads the PDF document in PDF Viewer
+            pdfViewer.Load("../../Data/HTTP Succinctly.pdf");
+
+            //Gets the filename of loaded PDF document
+            string fileName = pdfViewer.DocumentInformation.FileName;
+
+            //Gets the file path of loaded PDF document
+            string filePath = pdfViewer.DocumentInformation.FilePath;
+        }
+    }
+}
