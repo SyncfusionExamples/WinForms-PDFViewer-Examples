bfd2fc90859764ac2b8f8af187ecd047a3732e37
Yathavakrishnan Mohan <yathavakrishnan.mohan@syncfusion.com>
2024-10-03 15:05:15 +0530

    903535 - Feed back changes

--- Bookmarks Navigation/PDFBookmarks/PDFBookmarks/Properties/AssemblyInfo.cs
+++ Bookmarks Navigation/PDFBookmarks/PDFBookmarks/Properties/AssemblyInfo.cs
@@ -7,9 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 //[assembly: AssemblyTitle("SampleWF")]
 [assembly: AssemblyDescription("")]
-//[assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("")]
-//[assembly: AssemblyProduct("SampleWF")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,6 +28,3 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
