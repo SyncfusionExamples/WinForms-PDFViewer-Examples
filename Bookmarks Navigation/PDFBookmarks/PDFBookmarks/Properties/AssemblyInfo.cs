d1971d778a3f84e05defb6e839b98f4ae4305a22
Yathavakrishnan Mohan <yathavakrishnan.mohan@syncfusion.com>
2024-10-03 15:18:01 +0530

    903535 - Feed back changes

--- Bookmarks Navigation/PDFBookmarks/PDFBookmarks/Properties/AssemblyInfo.cs
+++ Bookmarks Navigation/PDFBookmarks/PDFBookmarks/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //[assembly: AssemblyTitle("SampleWF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("")] 
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
