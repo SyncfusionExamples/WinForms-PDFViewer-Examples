5f145c707b6eb6970c8d9d4c063ebd4941ed9e89
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-05-09 13:47:52 +0530

    949272 - Added core and framework sample for hiding toolbar button in Winforms PdfViewer

---
+++ How-to/Hide-Toolbar-Button/HideToolbarButton/HideToolbarButton/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9b6257ca-7b0f-47cd-9a5b-67a0d637a20e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
