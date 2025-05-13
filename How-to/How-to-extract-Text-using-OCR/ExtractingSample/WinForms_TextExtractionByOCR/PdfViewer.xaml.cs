9d84a18758a72b9da4286263c9928ade63a2910d
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-05-13 20:20:15 +0530

    removed unwanted code changes

--- How-to/How-to-extract-Text-using-OCR/ExtractingSample/WinForms_TextExtractionByOCR/PdfViewer.xaml.cs
+++ How-to/How-to-extract-Text-using-OCR/ExtractingSample/WinForms_TextExtractionByOCR/PdfViewer.xaml.cs
@@ -24,19 +24,11 @@ namespace WinForms_TextExtractionByOCR
     /// </summary>
     public partial class PdfViewer : UserControl
     {
-        string file;
-        string tessaractBinariesPath;
-        string tessDataPath;
         RectangleF bounds;
         public PdfViewer()
         {
             InitializeComponent();
-#if NETCOREAPP
-            file = "../../../Data/F#.pdf";
-#else
-            file = "../../Data/F#.pdf";
-#endif
-            pdfViewer.Load(file);
+            pdfViewer.Load("../../Data/F#.pdf");
         }
 
         private void Rectangle_Click(object sender, RoutedEventArgs e)
@@ -49,23 +41,16 @@ namespace WinForms_TextExtractionByOCR
         {
             if (e.Action == Syncfusion.Windows.PdfViewer.AnnotationChangedAction.Add)
             {
-#if NETCOREAPP
-                tessaractBinariesPath = "../../../Tesseract binaries";
-                tessDataPath = @"../../../Tessdata/";
-#else
-                tessaractBinariesPath = "../../Tesseract binaries";
-                tessDataPath = @"../../Tessdata/";
-#endif
                 bounds = e.NewBounds;
                 PdfLoadedDocument loadedDocument = pdfViewer.LoadedDocument;
-                using (OCRProcessor processor = new OCRProcessor(tessaractBinariesPath))
+                using (OCRProcessor processor = new OCRProcessor("../../Tesseract binaries"))
                 {
                     //Language to process the OCR
                     processor.Settings.Language = Languages.English;
                     Bitmap image = GetBitmap(pdfViewer.ExportAsImage(pdfViewer.CurrentPageIndex - 1));
                     using (Bitmap clonedImage = image.Clone(bounds, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
                     {
-                        string ocrText = processor.PerformOCR(clonedImage, tessDataPath);
+                        string ocrText = processor.PerformOCR(clonedImage, @"../../Tessdata/");
                     }
                     image.Dispose();
                 }
