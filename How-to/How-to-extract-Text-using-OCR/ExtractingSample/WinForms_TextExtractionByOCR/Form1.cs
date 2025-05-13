a97a653bb0d06faf3e7fadb490c1cb43e004df7e
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-05-13 20:02:10 +0530

    898833 - added sample for the extracting text using OCR

---
+++ How-to/How-to-extract-Text-using-OCR/ExtractingSample/WinForms_TextExtractionByOCR/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.Windows.PdfViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace WinForms_TextExtractionByOCR
+{
+    public partial class Form1 : Form
+    {
+        ElementHost elementHost = new ElementHost();
+        PdfViewer pdfViewer;
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            pdfViewer = new PdfViewer();
+            elementHost.Dock = DockStyle.Fill;
+            elementHost.Child = pdfViewer;
+            this.Controls.Add(elementHost);
+
+        }
+    }
+}
---
+++ How-to/How-to-extract-Text-using-OCR/ExtractingSample/WinForms_TextExtractionByOCR/PdfViewer.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Syncfusion.OCRProcessor;
+using Syncfusion.Pdf.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinForms_TextExtractionByOCR
+{
+    /// <summary>
+    /// Interaction logic for PdfViewer.xaml
+    /// </summary>
+    public partial class PdfViewer : UserControl
+    {
+        string file;
+        string tessaractBinariesPath;
+        string tessDataPath;
+        RectangleF bounds;
+        public PdfViewer()
+        {
+            InitializeComponent();
+#if NETCOREAPP
+            file = "../../../Data/F#.pdf";
+#else
+            file = "../../Data/F#.pdf";
+#endif
+            pdfViewer.Load(file);
+        }
+
+        private void Rectangle_Click(object sender, RoutedEventArgs e)
+        {
+            pdfViewer.AnnotationMode = Syncfusion.Windows.PdfViewer.PdfDocumentView.PdfViewerAnnotationMode.Rectangle;
+            pdfViewer.ShapeAnnotationChanged += PdfViewer_ShapeAnnotationChanged;
+        }
+
+        private void PdfViewer_ShapeAnnotationChanged(object sender, Syncfusion.Windows.PdfViewer.ShapeAnnotationChangedEventArgs e)
+        {
+            if (e.Action == Syncfusion.Windows.PdfViewer.AnnotationChangedAction.Add)
+            {
+#if NETCOREAPP
+                tessaractBinariesPath = "../../../Tesseract binaries";
+                tessDataPath = @"../../../Tessdata/";
+#else
+                tessaractBinariesPath = "../../Tesseract binaries";
+                tessDataPath = @"../../Tessdata/";
+#endif
+                bounds = e.NewBounds;
+                PdfLoadedDocument loadedDocument = pdfViewer.LoadedDocument;
+                using (OCRProcessor processor = new OCRProcessor(tessaractBinariesPath))
+                {
+                    //Language to process the OCR
+                    processor.Settings.Language = Languages.English;
+                    Bitmap image = GetBitmap(pdfViewer.ExportAsImage(pdfViewer.CurrentPageIndex - 1));
+                    using (Bitmap clonedImage = image.Clone(bounds, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+                    {
+                        string ocrText = processor.PerformOCR(clonedImage, tessDataPath);
+                    }
+                    image.Dispose();
+                }
+            }
+
+        }
+
+        Bitmap GetBitmap(BitmapSource source)
+        {
+            Bitmap bmp = new Bitmap(
+              source.PixelWidth,
+              source.PixelHeight,
+              System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            BitmapData data = bmp.LockBits(
+              new System.Drawing.Rectangle(System.Drawing.Point.Empty, bmp.Size),
+              ImageLockMode.WriteOnly,
+              System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            source.CopyPixels(
+              Int32Rect.Empty,
+              data.Scan0,
+              data.Height * data.Stride,
+              data.Stride);
+            bmp.UnlockBits(data);
+            return bmp;
+        }
+    }
+}
