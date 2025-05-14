e0105b041c22a211c32b122b67011e356a9ad69e
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-05-14 19:09:00 +0530

    900674 - added sample for apply localization to WPF PdfViewer in winforms app

---
+++ How-to/Apply-localization-to-WPF-PdfViewer-hosted-in-Winforms/HostedLocalizationSample/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.Windows.PdfViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace HostedLocalizationSample
+{
+    public partial class Form1 : Form
+    {
+        ElementHost elementHost = new ElementHost();
+        WPFUserControl pdfViewer;
+        public Form1()
+        {
+            InitializeComponent();
+            pdfViewer = new WPFUserControl();
+            elementHost.Dock = DockStyle.Fill;
+            elementHost.Child = pdfViewer;
+            this.Controls.Add(elementHost);
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
---
+++ How-to/Apply-localization-to-WPF-PdfViewer-hosted-in-Winforms/HostedLocalizationSample/WPFUserControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Syncfusion.Windows.PdfViewer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace HostedLocalizationSample
+{
+    /// <summary>
+    /// Interaction logic for WPFUserControl.xaml
+    /// </summary>
+    public partial class WPFUserControl : UserControl
+    {
+        string path;
+        public WPFUserControl()
+        {
+            InitializeComponent();
+            //set the current culture to the en
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+            //Set the custom class library assmebly and and namespace for the localization
+            LocalizationManager.SetResources(
+                typeof(LocalizationClassLibrary.Class1).Assembly,
+                "LocalizationClassLibrary"
+            );
+#if NETCOREAPP
+            path = "../../../Data/F#.pdf";
+#else
+            path = "../../Data/F#.pdf";
+#endif
+            //Load the pdf
+            pdfViewer.Load(path);
+
+        }
+    }
+}
