9965278b372008181fcb97d19e34df511fc5262b
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-06-11 13:00:55 +0530

    20537 - Added framework sample to silently print PDF

---
+++ How-to/Silent-print -PDF-in-dotnet_framework/SilentPrintPDF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Windows.Forms.PdfViewer;
+
+namespace SilentPrintPDF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load the PDF document 
+            PdfLoadedDocument ldoc = new PdfLoadedDocument(@"../../Data/F# Succinctly.pdf");
+            PdfViewerControl viewer = new PdfViewerControl();
+            //Load the PDFLoadedDocument in PDF viewer control
+            viewer.Load(ldoc);
+            Console.WriteLine("Printing started...");
+            //Silent printing the PDF document using the PDF viewer control
+            viewer.Print(false);
+            Console.WriteLine("Printing is done…");
+        }
+    }
+}
