9f0539c665247a79a57cead315c8009a69b34133
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-06-26 17:05:27 +0530

    feedbacks addressed

--- How-to/ViewPDFInWinforms/Form1.cs
+++ How-to/ViewPDFInWinforms/Form1.cs
@@ -1,36 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using Syncfusion.Windows.Forms.PdfViewer;
 
 namespace ViewPDFInWinforms
 {
     public partial class Form1 : Form
     {
-        string filePath;
         public Form1()
         {
             InitializeComponent();
+            //Opening the form in maximized state 
+            this.WindowState = FormWindowState.Maximized;
             //Initializing the PdfViewerControl
             PdfViewerControl pdfViewerControl1 = new PdfViewerControl();
-
             //Add PdfViewerControl to the Form
             Controls.Add(pdfViewerControl1);
             //Docking the control to all edges of its containing control and sizing appropriately.
             pdfViewerControl1.Dock = DockStyle.Fill;
-#if NETCOREAPP
-            filePath = @"../../../Data/PDF_Succinctly.pdf";
-#else
-            filePath = @"../../Data/PDF_Succinctly.pdf";
-#endif
             //Loading the document in the PdfViewerControl
-            pdfViewerControl1.Load(filePath);
+            pdfViewerControl1.Load(@"../../../Data/PDF_Succinctly.pdf");
         }
     }
 }
